7d569d371505fce2e71c3fc92f53246625dd172c
Andreas <trng111@sisu-login4.csc.fi>
2017-06-28 15:31:34 +0300

    Ex 6

--- exercises/programming/fortran/subroutines/laplacian.F90
+++ exercises/programming/fortran/subroutines/laplacian.F90
@@ -5,29 +5,33 @@ module laplacian_mod
 contains
   
   subroutine initialize(field0)
-	implicit none
+    
     real, intent(inout) :: field0(:,:)
-	integer :: nx=5, ny=5
+    integer :: s(2)
+
+    s=shape(field0)
 	
     field0 = 65.0
 
     field0(:,1) = 20.0
-    field0(:,ny) = 70.0
+    field0(:,s(2)) = 70.0
     field0(1,:) = 85.0
-    field0(nx,:) = 5.0
+    field0(s(1),:) = 5.0
   
   end subroutine initialize
 	
   subroutine laplacian(curr, prev)
-	implicit none
+
 	real, intent(in) :: prev(:,:)
 	real, intent(out) :: curr(:,:)
-	integer :: i, j
+	integer :: i, j, s(2)
 	
-	curr = prev
+	s=shape(prev)
 
-	do i = 2, nx-1
-		do j = 2, ny-1
+	curr = 0.0
+
+	do i = 2, s(1)-1
+		do j = 2, s(2)-1
 			curr(i,j) = (-2*prev(i,j) + prev(i-1,j) + prev(i+1,j))/dx**2  +&
 			& (- 2*prev(i,j) + prev(i,j-1) + prev(i,j+1))/dy**2
 		end do
@@ -36,13 +40,15 @@ contains
   end subroutine laplacian
 
   subroutine write_field(array)
-	implicit none
-	real, intent(in) :: A(:,:)
-	integer :: i
+	
+	real, intent(in) :: array(:,:)
+	integer :: i, s(2)
 
-	do i = 1, nx
-		write(*,*) A(i,:)
+	s=shape(array)
+
+	do i = 1, s(1)
+		write(*,*) array(i,:)
 	end do
-  end subroutine write_field(A)
+  end subroutine write_field
 	
 end module laplacian_mod
